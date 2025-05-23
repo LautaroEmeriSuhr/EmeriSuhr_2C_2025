ae50ae4ccfbe44f96b9214d6a98253d7d55b6d2a
Lautaro Emeri Suhr <lautaro.emeri@ingenieria.uner.edu.ar>
2025-05-23 14:24:06 -0300

    Se sube Actividad 4 del Proyecto 2

--- firmware/projects/Proyecto02_Actividad04/main/Proyecto02_Actividad04.c
+++ firmware/projects/Proyecto02_Actividad04/main/Proyecto02_Actividad04.c
@@ -34,17 +34,9 @@
 /*==================[macros and definitions]=================================*/
 
 TaskHandle_t tarea_conversor_adc_handle = NULL; /* Handle de la tarea de conversor ADC */
-TaskHandle_t tarea_lector_ecg_handle = NULL; /* Handle de la tarea de lector ECG */
+TaskHandle_t tarea_lector_ecg_handle = NULL;	/* Handle de la tarea de lector ECG */
 
 /*==================[internal data definition]===============================*/
-
-analog_input_config_t adc_config = {
-	.input = CH1,		/*!< Inputs: CH0, CH1, CH2, CH3 */
-	.mode = ADC_SINGLE, /*!< Mode: single read or continuous read */
-	.func_p = NULL,		/*!< Pointer to callback function for convertion end (only for continuous mode) */
-	.param_p = NULL,	/*!< Pointer to callback function parameters (only for continuous mode) */
-	.sample_frec = 0	/*!< Sample frequency min: 20kHz - max: 2MHz (only for continuous mode)  */
-};
 
 /*==================[internal functions declaration]=========================*/
 void FuncTimerA(void *param)
@@ -61,48 +53,41 @@ void FuncTimerB(void *param)
 
 uint16_t adc_value = 0; /* Variable para almacenar el valor del ADC */
 
-#define BUFFER_SIZE 231
-
 TaskHandle_t main_task_handle = NULL;
-const char ecg[BUFFER_SIZE] = {
-    76, 77, 78, 77, 79, 86, 81, 76, 84, 93, 85, 80,
-    89, 95, 89, 85, 93, 98, 94, 88, 98, 105, 96, 91,
-    99, 105, 101, 96, 102, 106, 101, 96, 100, 107, 101,
-    94, 100, 104, 100, 91, 99, 103, 98, 91, 96, 105, 95,
-    88, 95, 100, 94, 85, 93, 99, 92, 84, 91, 96, 87, 80,
-    83, 92, 86, 78, 84, 89, 79, 73, 81, 83, 78, 70, 80, 82,
-    79, 69, 80, 82, 81, 70, 75, 81, 77, 74, 79, 83, 82, 72,
-    80, 87, 79, 76, 85, 95, 87, 81, 88, 93, 88, 84, 87, 94,
-    86, 82, 85, 94, 85, 82, 85, 95, 86, 83, 92, 99, 91, 88,
-    94, 98, 95, 90, 97, 105, 104, 94, 98, 114, 117, 124, 144,
-    180, 210, 236, 253, 227, 171, 99, 49, 34, 29, 43, 69, 89,
-    89, 90, 98, 107, 104, 98, 104, 110, 102, 98, 103, 111, 101,
-    94, 103, 108, 102, 95, 97, 106, 100, 92, 101, 103, 100, 94, 98,
-    103, 96, 90, 98, 103, 97, 90, 99, 104, 95, 90, 99, 104, 100, 93,
-    100, 106, 101, 93, 101, 105, 103, 96, 105, 112, 105, 99, 103, 108,
-    99, 96, 102, 106, 99, 90, 92, 100, 87, 80, 82, 88, 77, 69, 75, 79,
-    74, 67, 71, 78, 72, 67, 73, 81, 77, 71, 75, 84, 79, 77, 77, 76, 76,
-};
+const char ecg[] = {
+	17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 18, 18, 18, 17, 17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 17, 17, 16, 16, 16, 16, 17, 17, 18, 18, 18, 17, 17, 17, 17,
+	18, 18, 19, 21, 22, 24, 25, 26, 27, 28, 29, 31, 32, 33, 34, 34, 35, 37, 38, 37, 34, 29, 24, 19, 15, 14, 15, 16, 17, 17, 17, 16, 15, 14, 13, 13, 13, 13, 13, 13, 13, 12, 12,
+	10, 6, 2, 3, 15, 43, 88, 145, 199, 237, 252, 242, 211, 167, 117, 70, 35, 16, 14, 22, 32, 38, 37, 32, 27, 24, 24, 26, 27, 28, 28, 27, 28, 28, 30, 31, 31, 31, 32, 33, 34, 36,
+	38, 39, 40, 41, 42, 43, 45, 47, 49, 51, 53, 55, 57, 60, 62, 65, 68, 71, 75, 79, 83, 87, 92, 97, 101, 106, 111, 116, 121, 125, 129, 133, 136, 138, 139, 140, 140, 139, 137,
+	133, 129, 123, 117, 109, 101, 92, 84, 77, 70, 64, 58, 52, 47, 42, 39, 36, 34, 31, 30, 28, 27, 26, 25, 25, 25, 25, 25, 25, 25, 25, 24, 24, 24, 24, 25, 25, 25, 25, 25, 25, 25,
+	24, 24, 24, 24, 24, 24, 24, 24, 23, 23, 22, 22, 21, 21, 21, 20, 20, 20, 20, 20, 19, 19, 18, 18, 18, 19, 19, 19, 19, 18, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 17, 17, 17, 17,
+	17, 17, 17};
 
 void tarea_conversor_adc(void *pvParameter)
 {
-	while(true){
-	ulTaskNotifyTake(pdTRUE, portMAX_DELAY);				  	/* Espera a que se le notifique */
-	AnalogInputReadSingle(adc_config.input, &adc_value);	  	/* Lee el valor del ADC */
-	UartSendString(UART_PC, ">ADCValue:");				  		/* Envía el mensaje */
-	UartSendString(UART_PC, (char *)UartItoa(adc_value, 10));  	/* Envía el valor del ADC */
-	UartSendString(UART_PC, "\r\n");						  	/* Envía un salto de línea */
+	while (true)
+	{
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);				  /* Espera a que se le notifique */
+		AnalogInputReadSingle(CH1, &adc_value);					  /* Lee el valor del ADC */
+		UartSendString(UART_PC, ">ADCValue:");					  /* Envía el mensaje */
+		UartSendString(UART_PC, (char *)UartItoa(adc_value, 10)); /* Envía el valor del ADC */
+		UartSendString(UART_PC, "\r\n");						  /* Envía un salto de línea */
 	}
 }
 
 void lector_ecg(void *pvParameter)
 {
-	while(true)
+	uint16_t i = 0;		   /* Variable para recorrer el buffer */
+	uint8_t ecg_value = 0; /* Variable para almacenar el valor del ECG */
+	while (true)
 	{
-		ulTaskNotifyTake(pdTRUE, portMAX_DELAY); /* Espera a que se le notifique */
-		for(int i=0; i<BUFFER_SIZE; i++)
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		ecg_value = ecg[i];			  /* Obtengo el valor del ECG */
+		AnalogOutputWrite(ecg_value); // escribo el valor y lo convierto en analógico
+		i++;
+		if (i == sizeof(ecg))
 		{
-			AnalogOutputWrite((uint8_t*) ecg[i]);	/* Envía el valor del ECG */
+			i = 0;
 		}
 	}
 }
@@ -110,7 +95,17 @@ void lector_ecg(void *pvParameter)
 /*==================[external functions definition]==========================*/
 void app_main(void)
 {
+
+	analog_input_config_t adc_config = {
+		.input = CH1,		/*!< Inputs: CH0, CH1, CH2, CH3 */
+		.mode = ADC_SINGLE, /*!< Mode: single read or continuous read */
+		.func_p = NULL,		/*!< Pointer to callback function for convertion end (only for continuous mode) */
+		.param_p = NULL,	/*!< Pointer to callback function parameters (only for continuous mode) */
+		.sample_frec = 0	/*!< Sample frequency min: 20kHz - max: 2MHz (only for continuous mode)  */
+	};
+
 	AnalogInputInit(&adc_config); /* Inicializa el ADC */
+	AnalogOutputInit();			  /* Inicializa el DAC */
 
 	timer_config_t timer_1 = {
 		.timer = TIMER_A,	  /*!< Selected timer */
@@ -119,28 +114,30 @@ void app_main(void)
 		.param_p = NULL		  /*!< Pointer to callback function parameter */
 	};
 
-timer_config_t timer_2 = {
+	TimerInit(&timer_1); /* Inicializa el timer */
+
+	timer_config_t timer_2 = {
 		.timer = TIMER_B,	  /*!< Selected timer */
-		.period = 20000,	  /*!< Period (in us) */
+		.period = 25000,	  /*!< Period (in us) */
 		.func_p = FuncTimerB, /*!< Pointer to callback function to call periodically */
 		.param_p = NULL		  /*!< Pointer to callback function parameter */
 	};
 
+	TimerInit(&timer_2); /* Inicializa el timer */
 
 	serial_config_t my_uart = {
-		.port = UART_PC,	/*!< port */
+		.port = UART_PC,	 /*!< port */
 		.baud_rate = 115200, /*!< baudrate (bits per second) */
-		.func_p = NULL,		/*!< Pointer to callback function to call when receiving data (= UART_NO_INT if not requiered)*/
-		.param_p = NULL		/*!< Pointer to callback function parameters */
+		.func_p = NULL,		 /*!< Pointer to callback function to call when receiving data (= UART_NO_INT if not requiered)*/
+		.param_p = NULL		 /*!< Pointer to callback function parameters */
 	};
 
-	TimerInit(&timer_1); /* Inicializa el timer */
-	UartInit(&my_uart);	 /* Inicializa el UART */
+	UartInit(&my_uart); /* Inicializa el UART */
+
+	xTaskCreate(&tarea_conversor_adc, "tarea_adc", 512, NULL, 5, &tarea_conversor_adc_handle);
+	xTaskCreate(lector_ecg, "lector_ecg", 2048, NULL, 1, &tarea_lector_ecg_handle);
 
 	TimerStart(timer_1.timer); /* Inicia el timer */
 	TimerStart(timer_2.timer); /* Inicia el timer */
-
-	xTaskCreate(tarea_conversor_adc,"tarea_conversor_adc", 2048, NULL, 1, &tarea_conversor_adc_handle);
-	xTaskCreate(lector_ecg,"lector_ecg", 2048, NULL, 1, &tarea_lector_ecg_handle);
 }
 /*==================[end of file]============================================*/
